090ffef9d78fe963497560dc5347593829e4ba1a
Kellen Baker <mkellenbaker@gmail.com>
2019-07-21 14:50:16 -0500

    add animation

--- src/Work/Work.js
+++ src/Work/Work.js
@@ -9,41 +9,62 @@ import { connect } from "react-redux";
 import "./Work.css";
 import pic from "../images/githubActivity.png";
 import kellenbaker from "../images/kellenbaker.jpg";
+import publicSpeaking from "../images/publicSpeaking.jpg";
 //semantic ui
 import { Card, Icon, Image } from "semantic-ui-react";
-import Grid from "@material-ui/core/Grid";
+import WOW from "wowjs";
+
 
 class Work extends Component {
+  componentDidMount() {
+    const wow = new WOW.WOW();
+    wow.init();
+  }
+
+  handleGithub = () => {
+    window.open("http://www.github.com/mkbaker", "_blank");
+  };
+
+  handleLinkedIn = () => {
+    window.open("https://www.linkedin.com/in/kellen-baker/", "_blank");
+  };
+
+  //this link needs to be changed upon creation of CV page
+  handleCV = () => {
+    this.props.history.push("/about");
+  };
+
   render() {
     return (
+      
       <div className="containerDiv">
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Card>
-              <Image src={pic} />
-              <Card.Content>
-                <Card.Header>GitHub Portfolio</Card.Header>
-                <Card.Description>
-                  Check out my projects on GitHub.
-                </Card.Description>
-              </Card.Content>
-            </Card>
-          </Grid>
-          
-          <Grid item xs={4}>
-            <Card>
-              <Image src={kellenbaker} />
-              <Card.Content>
-                <Card.Header>LinkedIn Profile</Card.Header>
-                <Card.Description>
-                  Feel free to connect!
-                </Card.Description>
-              </Card.Content>
-            </Card>
-          </Grid>
+        <Card onClick={this.handleGithub} className="wow flipInY">
+          <Image src={pic} />
+          <Card.Content>
+            <Card.Header>GitHub Portfolio</Card.Header>
+            <Card.Description>
+              Check out my projects on GitHub.
+            </Card.Description>
+          </Card.Content>
+        </Card>
 
+        <Card onClick={this.handleLinkedIn} className="wow flipInY">
+          <Image src={kellenbaker} />
+          <Card.Content>
+            <Card.Header>LinkedIn Profile</Card.Header>
+            <Card.Description>Feel free to connect!</Card.Description>
+          </Card.Content>
+        </Card>
 
-        </Grid>
+        <Card onClick={this.handleCV} className="wow flipInY">
+          <Image src={publicSpeaking} />
+          <Card.Content>
+            <Card.Header>Resume / CV</Card.Header>
+            <Card.Description>
+              View my resume and full CV here.
+            </Card.Description>
+          </Card.Content>
+        </Card>
       </div>
     );
   }
